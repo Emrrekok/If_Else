bb8833d42ec1d0f90e6d85a89ae12ea44ca8f65d
kok_y <kok_y@DESKTOP-DH8PS95>
2025-05-17 18:39:56 +0300

    Proje dosyası ekle.

---
+++ If_Else/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Lütfen bir sayı giriniz.");
+int sayı = Convert.ToInt32(Console.ReadLine());
+if(sayı > 10)
+{
+    Console.WriteLine("10' dan büyüktür");
+}
+else if(sayı == 10)
+{
+
+    Console.WriteLine("sayınız 10'a eşittir");
+}
+else if(sayı < 10)
+{
+    Console.WriteLine("sayu 10'dan küçütür");
+}
+    int BölümündenKalan = sayı % 2;
+if (BölümündenKalan == 0)
+{
+
+    Console.WriteLine("sayı çiftir");
+}
+else 
+{
+    Console.WriteLine("girdiğiniz sayı tektdir");
+
+}
